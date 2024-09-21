893f0ac9815aab4d8591269d07f4f546ae1b6a38
Your Name <you@example.com>
2024-09-22 02:37:24 +0530

    auth done

---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+
+//https://appwrite.io/docs/products/auth/quick-start
+
+// kaam ki chiz -> https://appwrite.io/docs/references/cloud/client-web/account
+
+import conf from '../conf/conf.js'
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client.setEndPoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+
+            if(userAccount){
+                // return userAccount
+                return this.login({email, password})
+            }   
+            else{
+                return userAccount;
+            }
+
+
+
+        } catch(error){
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logOut(){
+        try {
+             await this.account.deleteSessions();
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+const authService = AuthService();
+export default authService
